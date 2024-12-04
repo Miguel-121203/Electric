92c0db83701731eb54463ae45b04d0ff081e8155
Ikeroed04060 <ikeroed04060@gmail.com>
2024-12-04 17:06:01 -0500

    Mejora en imagenes del carrusel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { ViewportScroller, CommonModule } from '@angular/common'; // Importa los servicios necesarios
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule], // Incluye CommonModule
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'], // Corrige typo de 'styleUrl' a 'styleUrls'
 })
 export class AppComponent {
   title = 'im-electric';
+
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.viewportScroller.scrollToPosition([0, 0]); // Restaura el scroll al inicio
+      }
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,7 @@ export const routes: Routes = [
 
 ];
 
+
 export const appRoutingProviders = [
     provideRouter(routes)
 ];
